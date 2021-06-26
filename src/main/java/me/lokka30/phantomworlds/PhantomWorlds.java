b58086cc1a551e4285935268e0029d3b56ab68f1
lokka30 <lokka30@protonmail.com>
2021-06-26 09:27:14 +0800

    2.0.0 dev
    Took 6 minutes

--- src/main/java/me/lokka30/phantomworlds/PhantomWorlds.java
+++ src/main/java/me/lokka30/phantomworlds/PhantomWorlds.java
@@ -24,6 +24,17 @@ import java.util.HashSet;
  */
 public class PhantomWorlds extends JavaPlugin {
 
+    /*
+    TODO:
+     * Translate backslash character in world names as a space so world names with a space can be used in the plugin
+     * Vanish compatibility
+     *  don't send 'by' messages unless the sender is not a player / target can see the (player) sender
+     *  add vanish compatibility to 'teleport' subcommand
+     *  add ability to toggle vanish compatibility
+     * log in console (LogLevel:INFO) when a command is prevented due to a target player seemingly being vanished to the command sender.
+     * Consider creating a world management GUI.
+     */
+
     /**
      * If you have contributed code to the plugin,
      * add your name to the end of this list! :)
