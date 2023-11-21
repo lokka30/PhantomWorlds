7b5b02b703f2193c44db38bd47407447cb1abaf9
creatorfromhell <daniel.viddy@gmail.com>
2023-11-21 16:47:25 -0500

    Add options for adding potion effects on world change.

--- src/main/java/me/lokka30/phantomworlds/listeners/PlayerChangeWorldListener.java
+++ src/main/java/me/lokka30/phantomworlds/listeners/PlayerChangeWorldListener.java
@@ -18,12 +18,12 @@ package me.lokka30.phantomworlds.listeners;
  */
 
 import me.lokka30.phantomworlds.PhantomWorlds;
-import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 /**
  * PlayerChangeWorldListener
@@ -41,10 +41,37 @@ public class PlayerChangeWorldListener implements Listener {
 
   @EventHandler
   public void onChangeWorld(PlayerChangedWorldEvent event) {
-    final String cfgPath = "worlds-to-load." + event.getPlayer().getWorld().getName() + ".gameMode";
-    if(PhantomWorlds.instance().data.getConfig().contains(cfgPath)) {
-      final GameMode mode = GameMode.valueOf(PhantomWorlds.instance().data.getConfig().getString(cfgPath));
+    final String cfgPath = "worlds-to-load." + event.getPlayer().getWorld().getName();
+    if(PhantomWorlds.instance().data.getConfig().contains(cfgPath + ".gameMode")) {
+      final GameMode mode = GameMode.valueOf(PhantomWorlds.instance().data.getConfig().getString(cfgPath + ".gameMode"));
       event.getPlayer().setGameMode(mode);
+    }
+
+    final String cfgPrevPath = "worlds-to-load." + event.getFrom().getName();
+    if(PhantomWorlds.instance().getConfig().contains(cfgPrevPath + ".effects") &&
+            PhantomWorlds.instance().getConfig().isConfigurationSection(cfgPrevPath + ".effects")) {
+      for(final String effName : PhantomWorlds.instance().getConfig().getConfigurationSection(cfgPrevPath + ".effects").getKeys(false)) {
+
+        final PotionEffectType type = PotionEffectType.getByName(effName);
+        if(type != null) {
+          event.getPlayer().removePotionEffect(type);
+        }
+      }
+    }
+
+    if(PhantomWorlds.instance().getConfig().contains(cfgPath + ".effects") &&
+            PhantomWorlds.instance().getConfig().isConfigurationSection(cfgPath + ".effects")) {
+
+      for(final String effName : PhantomWorlds.instance().getConfig().getConfigurationSection(cfgPath + ".effects").getKeys(false)) {
+        final int duration = PhantomWorlds.instance().getConfig().getInt(cfgPath + ".effects." + effName, 60);
+        final int amplifier = PhantomWorlds.instance().getConfig().getInt(cfgPath + ".effects." + effName, 1);
+
+        final PotionEffectType type = PotionEffectType.getByName(effName);
+        if(type != null) {
+          final PotionEffect effect = new PotionEffect(type, duration, amplifier);
+          event.getPlayer().addPotionEffect(effect);
+        }
+      }
     }
   }
 }
